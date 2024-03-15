8855098066869ddb94db85a19377cf9978acd4d4
Stanislav N <stanislav_developer@mail.ru>
2024-03-15 19:40:21 +0500

    add animations

---
+++ Assets/Scripts/Model/Unit/RagdollController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Model.Unit
+{
+    public class RagdollController : MonoBehaviour
+    {
+        [SerializeField] private int i;
+        [SerializeField] private Animator _animator;
+        [SerializeField] private Rigidbody[] _bodyParts;
+
+        private void Awake()
+        {
+            foreach(var bodyPart in _bodyParts)
+                bodyPart.isKinematic = true;
+        }
+
+
+    }
+}
